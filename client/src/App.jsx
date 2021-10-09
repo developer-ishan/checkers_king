0b551b5aaf14574d2345bc5da17cf2f12cfb2477
lovedeep singh kamal <developer.lovedeep@gmail.com>
2021-10-09 14:05:46 +0530

    react app initialized

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="bg-gradient-to-r from-green-400 to-blue-500 h-full">
+      <header>
+        <p className="text-4xl text-center capitalize text-white">hello peter</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
