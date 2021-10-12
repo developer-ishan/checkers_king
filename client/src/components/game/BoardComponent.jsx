f040202b8fa945e88eaa44aa28751b5b442d2a87
lovedeep singh kamal <developer.lovedeep@gmail.com>
2021-10-12 11:59:58 +0530

    :sparkles: responsiveness and ui of board done

--- client/src/components/game/BoardComponent.jsx
+++ client/src/components/game/BoardComponent.jsx
@@ -103,6 +103,54 @@ const BoardComponent = ({ board, color, turn, movePiece }) => {
     }
   };
 
+  // for styling the boxes of the board
+  const whiteBoxStyle =
+    "grid relative place-items-center text-black bg-pink-300 ";
+  const blackBoxStyle =
+    "grid relative place-items-center text-white bg-indigo-600";
+
+  //this function will return one of the sytle
+  //from above depending upon coordinate
+  const getStyle = (i, j) => {
+    if ((i + j) % 2 === 0) return blackBoxStyle;
+    else return whiteBoxStyle;
+  };
+
+  //takes coordinate of a box as argument
+  //and return true if they are same as the selected box
+  //else return false
+  const isSelected = (currentI, currentJ) => {
+    if (selectedPiece.i === currentI && selectedPiece.j === currentJ)
+      return true;
+    return false;
+  };
+
+  //this function return piece
+  const getPiece = (pieceType) => {
+    switch (pieceType) {
+      case 0:
+        return;
+      case 1:
+        return <div className="z-20 w-5/6 bg-red-500 rounded-full h-5/6"></div>; //red pawn
+      case 2:
+        return <div className="z-20 w-5/6 bg-black rounded-full h-5/6"></div>; //black pawn
+      case 3:
+        return (
+          <div className="z-20 grid w-5/6 text-yellow-500 bg-red-500 rounded-full place-content-center h-5/6">
+            Q
+          </div>
+        ); //red queen
+      case 4:
+        return (
+          <div className="z-20 grid w-5/6 text-yellow-500 bg-black rounded-full place-content-center h-5/6">
+            Q
+          </div>
+        ); //black queen
+
+      default:
+        break;
+    }
+  };
   return (
     <div>
       <h1 className="m-2">YOUR COLOR :- {color}</h1>
@@ -113,22 +161,40 @@ const BoardComponent = ({ board, color, turn, movePiece }) => {
       ) : (
         <h1>Waiting For Opponent To Move...</h1>
       )}
+      {/* board start*/}
+      <div
+        style={{ height: "90vmin", width: "90vmin" }}
+        className="grid grid-cols-1 mx-auto grid-rows-8"
+      >
+        {/* small boxes of the board */}
+        {board.map((row, i) => (
+          <div key={i} className="grid grid-cols-8">
+            {row.map((piece, j) => (
+              <div
+                key={`${i} ${j}`}
+                // here we are getting the background color of these small boxes
+                className={getStyle(i, j)}
+                onClick={() => clickCell(i, j)}
+              >
+                {/* putting the playing piece */}
+                {piece !== 0 && getPiece(board[i][j])}
 
-      {board.map((row, i) => (
-        <div key={i} className="grid grid-cols-8 mx-24">
-          {row.map((piece, j) => (
-            <div
-              key={`${i} ${j}`}
-              className="m-0.5 p-6 text-black bg-gray-100 col"
-              onClick={() => clickCell(i, j)}
-            >
-              {piece !== 0 && <div>{player[board[i][j]]}</div>}
-              {isHighlighted({ i, j }) !== undefined && <p>HERE</p>}
-              {piece === 0 && <div>{`.`}</div>}
-            </div>
-          ))}
-        </div>
-      ))}
+                {/* if current box is highligted then putting a overlay on the box*/}
+                {isHighlighted({ i, j }) !== undefined && (
+                  <div className="absolute inset-0 bg-green-500 border-4 border-green-800"></div>
+                )}
+
+                {/*if current box is selected then putting a overlay on the box*/}
+                {isSelected(i, j) && (
+                  <div className="absolute inset-0 z-10 bg-yellow-500 border-4 border-yellow-800"></div>
+                )}
+              </div>
+            ))}
+          </div>
+        ))}
+        {/* small boxes end */}
+      </div>
+      {/* board end */}
       <h1>Selected Cell :- {`(${selectedPiece.i}, ${selectedPiece.j})`}</h1>
     </div>
   );
