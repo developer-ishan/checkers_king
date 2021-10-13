0d5527831cd14021c0b788aa29e7feab4d45c222
lovedeep singh kamal <developer.lovedeep@gmail.com>
2021-10-13 16:17:05 +0530

    added music on load

--- client/src/components/Landing/Home.jsx
+++ client/src/components/Landing/Home.jsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "./components/Navbar";
 import UserAction from "./components/UserAction";
-
+import HomeSong from "../../static/home.mp3";
 const Home = () => {
+  const [music] = useState(new Audio(HomeSong));
+  const playMusic = () => {
+    music.play();
+  };
+
+  // playMusic();
   return (
-    <div className="bg-gray-100 bg-red-300 ">
+    <div className="bg-gray-100 bg-red-300 " onMouseMove={() => playMusic()}>
       <Navbar />
       <div className="grid grid-cols-12">
         {/* left side */}
