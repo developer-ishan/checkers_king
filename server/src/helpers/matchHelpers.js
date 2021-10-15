d7f8e4e7d53cabc78beea162d6f4087b48440129
developer-ishan <ishan@mnnit.ac.in>
2021-10-15 18:14:28 +0530

    update rating bug resolved

--- server/src/helpers/matchHelpers.js
+++ server/src/helpers/matchHelpers.js
@@ -14,7 +14,7 @@ const saveMatch = async (
   endTime,
   messages
 ) => {
-  const ratingsDelta = updateRating(players[0].id, players[1].id);
+  const ratingsDelta = await updateRating(players[0].id, players[1].id);
   const game = new Match({
     players: [
       {
