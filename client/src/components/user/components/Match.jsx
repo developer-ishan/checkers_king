0c1b8f1024333ba8c1a69d454744919fa621cff8
lovedeep singh kamal <developer.lovedeep@gmail.com>
2021-10-17 00:34:27 +0530

    integrated previous matches into frontend

--- client/src/components/user/components/Match.jsx
+++ client/src/components/user/components/Match.jsx
@@ -1,27 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const Match = ({ live }) => {
+const Match = ({ matchId, players }) => {
   return (
     <div className="flex flex-col justify-around p-2 space-y-2 bg-white rounded shadow-lg sm:flex-row">
       <div className="flex flex-col items-center justify-around space-x-2 sm:flex-row">
         <p className="text-xs italic font-bold sm:text-sm">opponent:</p>
-        <h1 className="text-sm font-bold sm:text-xl">janidushman</h1>
+        <h1 className="text-sm font-bold sm:text-xl">{players[0].userName}</h1>
       </div>
-      {live ? (
-        <button
-          type="submit"
-          className="w-full max-w-sm p-2 font-medium text-white bg-indigo-500 rounded shadow hover:bg-purple-700"
-        >
-          ongoing
-        </button>
-      ) : (
-        <button
-          type="submit"
-          className="w-full max-w-sm p-2 font-medium text-white bg-indigo-500 rounded shadow hover:bg-purple-700"
-        >
-          watch replay
-        </button>
-      )}
+      <Link
+        to={`/replay/${matchId}`}
+        className="w-full max-w-sm p-2 font-medium text-white bg-indigo-500 rounded shadow hover:bg-purple-700"
+      >
+        watch replay
+      </Link>
     </div>
   );
 };
--- server/src/controllers/matchController.js
+++ server/src/controllers/matchController.js
@@ -42,7 +42,36 @@ exports.getMyMatches = async (req, res, next) => {
         .status(404)
         .json({ success: false, msg: "No Matches Found!!" });
     console.log(matches);
-    return res.json(matches);
+
+    //the above matches array contains lots of data
+    //so filtering it down to only required data
+    let filteredData = [];
+    matches.forEach((match) => {
+      //creating an object of userId:ratingChange to refrence it later
+      let ratingChange = {};
+      match.players.forEach((player) => {
+        ratingChange[player.userId] = player.delta;
+      });
+
+      //filtering player info
+      let playersInfo = [];
+      match.details.forEach((detail) => {
+        const { _id, username, rating } = detail;
+        let player = {
+          id: _id,
+          userName: username,
+          rating: rating,
+          ratingChange: ratingChange[_id], //here we are using the above created ratingChange object
+        };
+        playersInfo.push(player);
+      });
+
+      const { _id } = match;
+      //this _id is actullay the id of the db object
+      //not the id which was shared between friends
+      filteredData.push({ matchId: _id, players: playersInfo });
+    });
+    return res.json(filteredData);
   } catch (err) {
     console.log(err);
     return null;
