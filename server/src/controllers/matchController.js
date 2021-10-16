77b80167b487eaa2d148e1e7494a149dc14b34e2
developer-ishan <ishan@mnnit.ac.in>
2021-10-17 00:35:54 +0530

    get match by id controller

--- server/src/controllers/matchController.js
+++ server/src/controllers/matchController.js
@@ -1,4 +1,5 @@
 const Match = require("../models/Match");
+const ObjectId = mongoose.Types.ObjectId;
 
 exports.getMyMatches = async (req, res, next) => {
   try {
@@ -15,6 +16,49 @@ exports.getMyMatches = async (req, res, next) => {
       {
         $project: {
           matchId: 1,
+          startTime: 1,
+          endTime: 1,
+          players: 1,
+          pids: {
+            $map: {
+              input: "$players",
+              as: "id",
+              in: { $toObjectId: "$$id.userId" },
+            },
+          },
+        },
+      },
+      {
+        $lookup: {
+          from: "users",
+          localField: "pids",
+          foreignField: "_id",
+          as: "details",
+        },
+      },
+    ]);
+
+    if (!matches)
+      return res
+        .status(404)
+        .json({ success: false, msg: "No Matches Found!!" });
+    console.log(matches);
+    return res.json(matches);
+  } catch (err) {
+    console.log(err);
+    return null;
+  }
+};
+
+exports.getMatchById = async (req, res, next) => {
+  try {
+    const {matchId} = req.params;
+    const matches = await Match.aggregate([
+      {
+        $match: {_id: ObjectId(matchId)}
+      },
+      {
+        $project: {
           startTime: 1,
           endTime: 1,
           players: 1,
