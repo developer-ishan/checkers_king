43d5ce43e17b8ec4adf68f8fedf09c08dcc24a2b
lovedeep singh kamal <developer.lovedeep@gmail.com>
2021-10-21 16:40:24 +0530

    added modal to create game

--- client/src/components/Landing/components/CreateGame.jsx
+++ client/src/components/Landing/components/CreateGame.jsx
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { useHistory } from "react-router";
 import { isAuthenticated } from "../../../helper/authHelper";
 import GameButton from "../../game/GameButton";
-
+import PlayWithFriendsOptionsModal from "../../modal/PlayWithFriendsOptionsModal";
 const CreateGame = ({ socket }) => {
   let history = useHistory();
-  // creates a new and redirects us to the game board
-  const createGame = () => {
+  const [isGameOptionModalOpen, setIsGameOptionModalOpen] = useState(false);
+  const [gameOptions, setGameOptions] = useState({
+    checker: "Red",
+    forceJump: true,
+    isRated: true,
+  });
+
+  const handleCreateGame = () => {
     const token = isAuthenticated();
-    // ARGS :- ("create-game", isBot, mandatoryMoves, token)
-    socket.emit("create-game", false, 4, "Red", true, true, token);
+    const { checker, forceJump, isRated } = gameOptions;
+    // ARGS :- ("create-game" ,isBot, botLevel, color, mandatoryMoves, isRated, token)
+    socket.emit("create-game", false, 4, checker, forceJump, isRated, token);
     history.push("/game");
+  };
+  const scrollOnClose = (val) => {
+    console.log("workded", val);
+    console.log(window.scrollTo(0, val));
   };
   return (
     <div className="relative grid w-full h-64 bg-white sm:h-full sm:rounded-lg sm:shadow-xl place-content-center">
@@ -22,10 +33,18 @@ const CreateGame = ({ socket }) => {
       <div className="absolute inset-0 rounded opacity-70 indigo-gradient dark:dark-gradient dark:bg-opacity-100 "></div>
       <GameButton
         className="absolute p-2 font-bold text-indigo-500 transform bg-white top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4"
-        onClickFn={() => createGame()}
+        onClickFn={() => setIsGameOptionModalOpen(true)}
       >
         New Game
       </GameButton>
+      <PlayWithFriendsOptionsModal
+        modalState={isGameOptionModalOpen}
+        setModalState={setIsGameOptionModalOpen}
+        gameOptions={gameOptions}
+        setGameOptions={setGameOptions}
+        scrollOnClose={scrollOnClose}
+        handleCreateGame={handleCreateGame}
+      />
     </div>
   );
 };
