6290ddec741c0ec4fdfee773a58b9ab73cd0e96a
lovedeep singh kamal <developer.lovedeep@gmail.com>
2021-12-14 22:57:09 +0530

    replaced old offered-draw modal with the new one

--- client/src/components/modal/DrawModal.jsx
+++ client/src/components/modal/DrawModal.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import Modal from "react-modal";
 import { useHistory } from "react-router-dom";
+import SmallScreenInfoModal from "./SmallScreenInfoModal";
 
 const DrawModal = ({ modalState, setModalState, socket, gameId }) => {
   let history = useHistory();
@@ -20,31 +21,26 @@ const DrawModal = ({ modalState, setModalState, socket, gameId }) => {
   };
 
   return (
-    <Modal
-      className="absolute z-50 w-2/5 transform top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4"
-      isOpen={modalState}
-      onRequestClose={closeModal}
+    <SmallScreenInfoModal
+      modalState={modalState}
+      setModalState={setModalState}
+      title="opponent offered draw"
     >
-      <div className="z-50 p-4 bg-gray-500">
-        <h1 className="text-center text-white capitalize text-md sm:text-2xl">
-          opponent offered draw
-        </h1>
-        <div className="flex flex-col items-center justify-around p-2 md:flex-row ">
-          <button
-            className="w-full max-w-xs px-4 py-2 m-2 text-xs font-bold text-white uppercase transition-all duration-150 bg-red-500 rounded shadow outline-none active:bg-red-600 hover:shadow-md hover:bg-red-600 focus:outline-none ease"
-            onClick={() => rejectDraw()}
-          >
-            cancel
-          </button>
-          <button
-            className="w-full max-w-xs px-4 py-2 m-2 text-xs font-bold text-white uppercase transition-all duration-150 bg-indigo-500 rounded shadow outline-none active:bg-red-600 hover:shadow-md hover:bg-indigo-600 focus:outline-none ease"
-            onClick={() => acceptDraw()}
-          >
-            accept
-          </button>
-        </div>
+      <div className="flex flex-col items-center justify-around p-2 md:flex-row ">
+        <button
+          className="w-full max-w-xs px-4 py-2 m-2 text-xs font-bold text-white uppercase transition-all duration-150 bg-indigo-500 rounded shadow outline-none active:bg-red-600 hover:shadow-md hover:bg-indigo-600 focus:outline-none ease"
+          onClick={() => acceptDraw()}
+        >
+          accept
+        </button>
+        <button
+          className="w-full max-w-xs px-4 py-2 m-2 text-xs font-bold text-white uppercase transition-all duration-150 bg-red-500 rounded shadow outline-none active:bg-red-600 hover:shadow-md hover:bg-red-600 focus:outline-none ease"
+          onClick={() => rejectDraw()}
+        >
+          cancel
+        </button>
       </div>
-    </Modal>
+    </SmallScreenInfoModal>
   );
 };
 
