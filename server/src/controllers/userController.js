b7a83e69d0eaa341d42fc1e53eaad4e5e001cf89
developer-ishan <ishan@mnnit.ac.in>
2021-12-15 04:52:47 +0530

    image bug fixed

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -2,6 +2,7 @@ const { check } = require("express-validator");
 const { userDpStore } = require("../config/multrStore");
 const User = require("../models/User");
 const multer = require("multer");
+const keys = require("../config/keys");
 exports.deleteUser = async (req, res, next) => {
   try {
     req.user.active = false;
@@ -31,11 +32,18 @@ exports.getMySummary = async (req, res, next) => {
     val.username = req.user.username;
     val._id = req.user._id;
     val.active = req.user.active;
-    if (req.user.google) val.g_photo = req.user.google.photo;
-    if (req.user.facebook) val.f_photo = req.user.facebook.photo;
+    if (req.user.photo) {
+      val.photo = keys.SERVER_BASE+"/public/dp/"+req.user.photo;
+    } else if (req.user.facebook) {
+      val.photo = req.user.facebook.photo;
+    } else if (req.user.google) {
+      val.photo = req.user.google.photo;
+    }
     val.desc = req.user.desc;
+    console.log(val);
     res.json(val);
   } catch (err) {
+    console.log(err);
     res.status(500).json(err);
   }
 };
