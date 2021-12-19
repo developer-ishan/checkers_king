dab9dca0f296aa30b2f90a5440d4e1100d78946e
developer-ishan <ishan@mnnit.ac.in>
2021-12-19 21:07:21 +0530

    Friends get apis completed

--- server/src/routes/friendRouter.js
+++ server/src/routes/friendRouter.js
@@ -1,9 +1,13 @@
-const { getMyFriends } = require("../controllers/friendController");
+const {
+  getMyFriends,
+  getMyFriendRequests,
+} = require("../controllers/friendController");
 const userAuth = require("../middleware/userAuth");
 const isVerified = require("../middleware/isVerified");
 
 const friendRouter = require("express").Router();
 
 friendRouter.get("/", userAuth, isVerified, getMyFriends);
+friendRouter.get("/requests", userAuth, isVerified, getMyFriendRequests);
 
 module.exports = friendRouter;
