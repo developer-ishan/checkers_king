c794f6d5388db24188230cf0dd2e3773262ff2bc
developer-ishan <ishan@mnnit.ac.in>
2021-12-20 02:14:00 +0530

    auto complete component made and tested, requires frontend work

--- client/src/components/comm/AutoCompleteInput.jsx
+++ client/src/components/comm/AutoCompleteInput.jsx
@@ -9,6 +9,7 @@ const AutoCompleteInput = ({ url, selected, setSelected }) => {
   return (
     <div>
       <input
+        className="g-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
         value={input}
         onChange={(e) => {
           setInput(e.target.value);
@@ -40,7 +41,7 @@ const AutoCompleteInput = ({ url, selected, setSelected }) => {
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
               key={user._id}
               onClick={(e) => {
-                  setSelected(user);
+                setSelected(user);
               }}
             >
               {user.username}
