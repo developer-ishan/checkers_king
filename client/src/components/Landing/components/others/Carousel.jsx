7adbe51752d970c99efc1ca95a7b9df881da861e
lovedeep singh kamal <developer.lovedeep@gmail.com>
2021-12-25 17:38:55 +0530

    added find user component to homepage and changed UI accordingly

---
+++ client/src/components/Landing/components/others/Carousel.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Carousel = () => {
+  return (
+    <div className="bg-white rounded-lg shadow-xl">
+      <p>carousel</p>
+    </div>
+  );
+};
+
+export default Carousel;
--- client/src/components/friend/components/OnlineFriends.jsx
+++ client/src/components/friend/components/OnlineFriends.jsx
@@ -14,16 +14,64 @@ const RequestList = ({ heading }) => {
     });
     return () => {
       socket.off("friend-online");
-    }
+    };
   }, []);
   return (
-    <div>
-      <h1>Online Friends</h1>
-      <ul>
+    <div
+      data-title="ONLINE FRIENDS"
+      data-intro="list of people who are online, you can invite them for game or can watch their game"
+      className="flex-grow w-full bg-white rounded-lg shadow-xl dark:bg-gray-700 lg:mx-auto lg:w-10/12"
+    >
+      <div className="p-4 bg-pink-500 rounded-t-lg dark:bg-gray-800">
+        <h2 className="text-lg font-semibold tracking-wide text-center text-white capitalize">
+          online friends
+        </h2>
+      </div>
+      <div className="p-4 pt-0 text-gray-800 divide-y divide-gray-400 rounded-b-lg dark:text-white ">
         {friends.map((f) => (
-          <li>{f.username}</li>
+          <div
+            className="flex flex-row items-center justify-between py-4"
+            data-title="players info"
+            data-intro="you can click on the name to see more info"
+          >
+            <img
+              src="/images/default.png"
+              alt="user-1"
+              className="w-12 h-12 rounded-full"
+            />
+            <div className="text-sm">
+              <span className="block font-semibold">{f.username}</span>
+              <span className="block text-xs font-light text-gray-700 dark:text-white">
+                Internation grand master
+              </span>
+            </div>
+            <a
+              href="#"
+              className="self-start block px-2 py-px mt-1 text-xs font-semibold tracking-wide uppercase border border-gray-400 rounded-full hover:bg-indigo-400 hover:text-white hover:border-transparent"
+            >
+              invite
+            </a>
+          </div>
         ))}
-      </ul>
+        {friends.length === 0 && (
+          <div className="text-center capitalize">
+            <p className="p-2 text-sm font-bold dark:text-white">
+              no friend online
+            </p>
+            <img
+              src="/images/alone-cat.png"
+              alt=""
+              className="w-full max-w-sm mx-auto"
+            />
+            <p className="p-2 text-sm font-bold dark:text-white">
+              looks like everyone is busy today
+            </p>
+            <p className="p-2 text-sm font-bold dark:text-white">
+              you can still play with random oniline players
+            </p>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
